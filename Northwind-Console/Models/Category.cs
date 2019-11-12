a2919bb80841745ef519dc6aa49810e736f176d1
amahdavi9503 <amahdavi9503@gmail.com>
2019-11-12 13:50:40 -0600

    Data validation using ANNOTATIONS
    CategoryName is required

--- Northwind-Console/Models/Category.cs
+++ Northwind-Console/Models/Category.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NorthwindConsole.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
+
+        [Required]  //CategoryName is required _ this is an annotation
         public string CategoryName { get; set; }
         public string Description { get; set; }
 
--- Northwind-Console/Program.cs
+++ Northwind-Console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using NLog;
 using NorthwindConsole.Models;
@@ -40,7 +42,24 @@ namespace NorthwindConsole
                         category.CategoryName = Console.ReadLine();
                         Console.WriteLine("Enter the Category Description:");
                         category.Description = Console.ReadLine();
-                        // TODO: save category to db
+
+                        // Save category to db  ***********?????? explain the following code
+                        ValidationContext context = new ValidationContext(category, null, null);
+                        List<ValidationResult> results = new List<ValidationResult>();
+
+                        var isValid = Validator.TryValidateObject(category, context, results, true);  //???????****
+                        if (isValid)
+                        {
+                            logger.Info("Validation passed");
+                            // TODO: save category to db
+                        }
+                        if (!isValid)
+                        {
+                            foreach (var result in results)
+                            {
+                                logger.Error($"{result.MemberNames.First()} : {result.ErrorMessage}");  //Create a list of errors if we have multiple errors
+                            }
+                        }
                     }
                     Console.WriteLine();
 
