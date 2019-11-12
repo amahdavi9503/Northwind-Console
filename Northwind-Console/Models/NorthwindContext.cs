502ab31e3494b43a5c4e350201f5665249c95ae2
amahdavi9503 <amahdavi9503@gmail.com>
2019-11-12 12:54:59 -0600

    Northwind Model
    1) Add Models Folder
    2) Create Supplier Class
    3) Create Category Class
    4) Create Product Class
    5) Create Northwind Context Class

---
+++ Northwind-Console/Models/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace NorthwindConsole.Models
+{
+    public class NorthwindContext : DbContext
+    {
+        //"base": We're using NorthwindContext with all of its default behaviors other than we've assigned a name to it
+        public NorthwindContext() : base("name=NorthwindContext") { } 
+
+        //Establish a relationship between our classes and tables in the database
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
